c3f093b152dc3c06bdfb3dc1501ab56d120371b2
MAXIMEDANTE\Owner <maximekanyinda@gmail.com>
2021-03-23 23:38:52 +0200

    initial commit and major app set up

---
+++ README.md
@@ -0,0 +1,25 @@
+# dev_to_clone
+## Project Description
+```
+This project is a clone of the Dev.to website. The project is built with Nuxt.js and uses the Dev.to API which is now available to the public. The UI is based on neumorphism.
+
+You can read more about neumorphism here [https://uxdesign.cc/neumorphism-in-user-interfaces-b47cef3bf3a6]
+```
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ components/TheFooter.vue
@@ -0,0 +1,59 @@
+<template>
+  <footer>
+    <span>Build with</span>
+    <a href="https://nuxtjs.org" target="_blank">
+      <nuxt-icon class="nuxt-icon" />
+    </a>
+    <span>&</span>
+    <a href="https://docs.dev.to/api" rel="nofollow noopener" target="_blank">
+      <dev-to-icon />
+    </a>
+    <span>&</span>
+    <span> By Maxime 🚀</span>
+  </footer>
+</template>
+
+<script>
+import DevToIcon from '@/assets/icons/dev-to.svg?inline'
+import NuxtIcon from '@/assets/icons/nuxt.svg?inline'
+export default {
+  components: {
+    DevToIcon,
+    NuxtIcon
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 2rem;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  span {
+    display: inline-block;
+    line-height: 1;
+    text-transform: uppercase;
+    letter-spacing: $-ls2;
+    font-size: $text-xs;
+    font-weight: $bold-body-font-weight;
+  }
+  span:last-child{
+    margin-left: 0.5rem;
+    text-transform: capitalize;
+  }
+  a {
+    svg {
+      width: 3rem;
+      height: 3rem;
+      margin: 0 0.5rem;
+    }
+    .nuxt-icon {
+      width: 2.5rem;
+      height: 2.5rem;
+      margin: 0 0.25rem;
+    }
+  }
+}
+</style>
